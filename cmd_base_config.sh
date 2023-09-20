b93995a35f0f99881b1878119d7104747f190981
v <zh1024@zohomail.cn>
2023-09-20 15:25:30 +0800

    update

--- cmd_base_config.sh
+++ cmd_base_config.sh
@@ -8,8 +8,9 @@ cp_conf_home ".config/environment.d/xdg.conf"
 cp_conf_home ".config/environment.d/cmd.conf"
 
 # 默认用户目录使用英文
-mkdir -p $HOME/{Desktop,Downloads,Templates,Public,Documents,Music,Pictures,Videos}
-cp_conf_home ".config/user-dirs.locale"
-cp_conf_home ".config/user-dirs.dirs"
-sed -i 's@$HOME@'$HOME'@g' "$HOME/.config/user-dirs.dirs"
-# 重启后在弹出窗选择"保留旧的名称(不要再询问我)"
+# mkdir -p $HOME/{Desktop,Downloads,Templates,Public,Documents,Music,Pictures,Videos}
+# cp_conf_home ".config/user-dirs.locale"
+# cp_conf_home ".config/user-dirs.dirs"
+# sed -i 's@$HOME@'$HOME'@g' "$HOME/.config/user-dirs.dirs"
+# https://wiki.archlinux.org/title/XDG_user_directorie
+LC_ALL=C xdg-user-dirs-update --force
--- default_path.sh
+++ default_path.sh
@@ -3,6 +3,7 @@
 ENV_PATH="$HOME/.config/environment.d"
 BIN_PATH="$HOME/.local/bin"
 APP_PATH="$HOME/.local/app"
-DOWN_PATH="$HOME/Downloads"
+# DOWN_PATH="$HOME/Downloads"
+DOWN_PATH=$(xdg-user-dir DOWNLOAD)
 
 NOTE_PATH="$HOME/asset/note"
