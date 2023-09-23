bcf7bde923b222a482ab4600f9d15c38332b1902
v <zh1024@zohomail.cn>
2023-09-23 23:40:45 +0800

    update

--- neovim.sh
+++ neovim.sh
@@ -5,15 +5,11 @@ source ./cp_conf.sh
 source ./default_path.sh
 
 # 先安装 nodejs pip cargo, 并换好各自的国内源, 然后 git 配置 https 和 raw 的国内代理
-
 install python3-all python3-pynvim fd-find ripgrep
-# node 的插件让脚本装 其它的自己用包管理器装
-# npm i -S tree-sitter-cli neovim --registry https://registry.npm.taobao.org/
-# 如果有网络问题 (clash 需要开启 tun 模式) 可以手动添加 proxy 后运行安装脚本
-# nvim $HOME/.local/share/yarn/global/node_modules/tree-sitter-cli
-# node install.js
 
+# 使用安装脚本安装(只需要让脚本安装 nodejs 的依赖, python 的 pip pynvim 和 fd 提前用包管理器安装)
 xdg-open https://github.com/LunarVim/LunarVim
 xdg-open https://www.lunarvim.org/docs/installation
 
-cp ~/.local/share/lunarvim/lvim/utils/installer/config.example.lua ~/.config/lvim/config.lua
+# 安装完有报错时启动lvim, 如果没有自动安装插件输入 :Lazy sync 安装插件
+# cp ~/.local/share/lunarvim/lvim/utils/installer/config.example.lua ~/.config/lvim/config.lua
