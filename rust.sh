de880f471eae3d38a33a5b18c3641777c6af90f3
v <zh1024@zohomail.cn>
2023-09-24 16:38:15 +0800

    update

--- rust.sh
+++ rust.sh
@@ -10,5 +10,9 @@ cp_conf_home ".cargo/config"
 # export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 cp_conf_home ".config/environment.d/rust.conf"
 
-install rust-all
+#install rust-all
+export RUSTUP_DIST_SERVER="https://rsproxy.cn"
+export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
+bash <(curl -s https://cdn.jsdelivr.net/gh/rust-lang-nursery/rustup.rs/rustup-init.sh)
+#rustup default stable # 需要重载 shell
 # rustup toolchain install <toolchain_name>
