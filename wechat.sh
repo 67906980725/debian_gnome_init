7f4d7528665d00f0b06931ec4d88f634e5d8cd8f
v <zh1024@zohomail.cn>
2023-10-02 12:55:19 +0800

    update

--- wechat.sh
+++ wechat.sh
@@ -11,4 +11,8 @@ sudo apt-get install com.qq.weixin.deepin
 # sudo ln -s /opt/apps/com.qq.weixin.deepin/entries/applications/com.qq.weixin.deepin.desktop /usr/share/applications/
 cp_conf_home ".local/share/applications/com.qq.weixin.deepin.desktop"
 
+# 如果无法发送照片
+sudo apt install libjpeg62:i386
+
+# 修改 wine 设置
 # env WINEPREFIX="$HOME/.deepinwine/Deepin-WeChat" deepin-wine6-stable winecfg
