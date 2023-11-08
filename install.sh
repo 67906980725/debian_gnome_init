5eecfa6cfd914a4100306e6f35b2e223e694c199
v <zh1024@zohomail.cn>
2023-11-08 20:06:29 +0800

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ install()
     sudo apt install "$@"
 }
 
-# https://ghproxy.net/ https://gh-proxy.com/
-# 发生变更时需要同步修改 $HOME/.config/environment.d/cmd.conf
-GITHUB_PROXY="${GITHUB_PROXY:-https://ghproxy.net/}"
+# github proxy
+source ./conf/home/.config/environment.d/cmd.conf
