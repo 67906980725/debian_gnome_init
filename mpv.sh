883412332203aada5caca9cfac09e039fac2e5db
v <zh1024@zohomail.cn>
2023-11-21 15:45:35 +0800

    update

--- mpv.sh
+++ mpv.sh
@@ -4,6 +4,7 @@ source ./install.sh
 source ./cp_conf.sh
 
 install mpv
+sudo sed -i 's/^Exec=/Exec=gnome-session-inhibit /' /usr/share/applications/mpv.desktop
 
 cp_conf_home ".config/mpv"
 # mpv实现简单音乐/视频音乐随机播放 (需要修改音乐目录)
