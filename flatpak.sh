ed150d20f2cb435ff9c6b675ffd780e3703876d7
v <v>
2024-03-08 09:25:49 +0800

    update

--- flatpak.sh
+++ flatpak.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 source ./util_common.sh
 
-install flatpak
+install flatpak gnome-software-plugin-flatpak
 
+# 国内源
 flatpak remote-add --if-not-exists flathub https://mirror.sjtu.edu.cn/flathub/flathub.flatpakrepo
 flatpak remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
 flatpak repair
 
-sudo flatpak install org.freedesktop.Platform.GL.default # 20.08
-flatpak install org.freedesktop.Platform.openh264 org.gnome.Platform.Locale org.gnome.Platform
-flatpak install com.github.tchx84.Flatseal # 权限管理
-flatpak install io.github.flattool.Warehouse # 应用管理
+# 需要重启
+sudo reboot
 
-flatpak install com.qq.QQ
+# 不需要 下面的软件有依赖会自己安装
+# sudo flatpak install org.freedesktop.Platform.GL.default # 20.08
+# flatpak install org.freedesktop.Platform.openh264 org.gnome.Platform.Locale org.gnome.Platform
+
+# 权限管理
+flatpak install com.github.tchx84.Flatseal
+# 应用管理
+flatpak install io.github.flattool.Warehouse
+
+# qq
+flatpak install com.qq.QQ 
 
--- vmware_pro.sh
+++ vmware_pro.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source ./util_common.sh
 
-# 相比virtualbox优势: 更流畅;双向剪切板不会出问题;gnome下ctrl+alt+方向/super+鼠标滚轮可以正常切换主机工作空间
+# 相比virtualbox优势: 双向剪切板不会出问题;gnome下ctrl+alt+方向/super+鼠标滚轮可以正常切换主机工作空间
 # 下载链接: https://www.vmware.com/go/getworkstation-linux
 # 使用管理员身份运行
 # 17产品密钥: MC60H-DWHD5-H80U9-6V85M-8280D
