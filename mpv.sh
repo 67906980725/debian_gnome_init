d71283f74cb7d156f9798cd4d1ae186757d9970c
v <v>
2024-03-14 11:53:19 +0800

    update

--- mpv.sh
+++ mpv.sh
@@ -2,9 +2,12 @@
 source ./util_common.sh
 
 install mpv
+
 # 防止播放中自动熄/锁屏
 sudo sed -i 's/^Exec=/Exec=gnome-session-inhibit /' /usr/share/applications/mpv.desktop
 
+# 默认设置, 参考自 https://zhuanlan.zhihu.com/p/372056999
 cp_conf_home ".config/mpv"
+
 # mpv实现简单音乐/视频音乐随机播放 (需要修改音乐目录)
 cp_conf_home ".local/script/mp"
