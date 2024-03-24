a1d6d750402093d0b1613363acc824c59bd87b6d
v <v>
2024-03-24 10:44:54 +0800

    update

--- wechat.sh
+++ wechat.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 source ./util_common.sh
 
+## origin
+# https://github.com/lovechoudoufu/wechat_for_linux
+
+
+## wine
 # http://www.taodudu.cc/news/show-5412804.html?action=onClick
 # https://github.com/zq1997/deepin-wine
 # 连接失败多试几次
